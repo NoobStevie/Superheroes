046e259ecda0fd4dfa23911db08dd08d62721588
Stevie <kamaustevek78@gmail.com>
2023-07-03 14:50:16 +0300

    Seeds Populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+hero1 = Hero.create(name: "Steve Rogers", super_name: "Captain America")
+hero2 = Hero.create(name: "Brie Larson", super_name: "Captain Marvel")
+hero3 = Hero.create(name: "Benedict Cumberbatch", super_name: "Dr Strange")
+
+power1 = Power.create(name: "Super Strength", description: "Gives the wielder super-human strengths")
+power2 = Power.create(name: "Flight", description: "Gives the wielder ability to fly through skies at supersonic speed")
+power3 = Power.create(name: "Super Senses", description: "Gives the wielder ability to sense danger from afar")
+
+hero_power1 = HeroPower.create(hero: hero1, power: power1, strength: "Strong")
+hero_power2 = HeroPower.create(hero: hero1, power: power3, strength: "Weak")
+hero_power3 = HeroPower.create(hero: hero2, power: power2, strength: "Average")
